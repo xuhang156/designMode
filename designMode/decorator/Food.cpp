953fead46f1f7e914970d99111384d422c07710d
徐航 | Hang Xu <xu.hang1@mech-mind.net>
2023-12-05 14:52:51 +0800

    feat：新增抽象构建类，用于具体构建和抽象装饰器共同继承

---
+++ designMode/decorator/Food.cpp
@@ -0,0 +1,8 @@
+#include <QDebug>
+#include "Food.h"
+
+Food::Food() {}
+
+void Food::cooking() { qDebug() << "Is cooking food."; }
+
+Food::~Food() { qDebug() << "Finished eating."; }
---
+++ designMode/decorator/Food.h
@@ -0,0 +1,13 @@
+#pragma once
+
+///
+/// \brief The Food class
+/// 抽象构建，用于具体构建（饼）和抽象装饰器（辅料：肉、菜）继承公共接口
+///
+class Food
+{
+public:
+    Food();
+    virtual ~Food();
+    virtual void cooking();
+};
