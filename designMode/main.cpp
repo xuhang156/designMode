e993fe98b96bcb751c92d102e8f9d874f83ed291
徐航 | Hang Xu <xu.hang1@mech-mind.net>
2023-12-05 15:28:34 +0800

    feat：使用装饰器模式

--- designMode/main.cpp
+++ designMode/main.cpp
@@ -1,7 +1,17 @@
 #include <QApplication>
+#include "decorator/Laobing.h"
+#include "decorator/Pork.h"
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+    Food* laobing = new Laobing();
+    Food* pork = new Pork(*laobing);
+    laobing->cooking();
+    pork->cooking();
+
+    delete laobing;
+    delete pork;
+
     return a.exec();
 }
